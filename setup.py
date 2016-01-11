5d5144e3dd7dfe8e49dc654a2aa79f24260f0aa0
Leon <i@leons.im>
2016-01-11 18:17:23 +0800

    Fix setup script.

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup, find_packages
 setup(
     name = 'segmenttree',
     version = '1.0.0',
-    keywords = ('segment', 'tree'),
+    keywords = 'segment tree',
     description = 'A Python implementation of Segment Tree',
     license = 'MIT License',
 
     url = 'https://leons.im/posts/a-python-implementation-of-segment-tree/',
-    author = 'Leon S',
+    author = '1e0n',
     author_email = 'i@leons.im',
 
     packages = find_packages(),
