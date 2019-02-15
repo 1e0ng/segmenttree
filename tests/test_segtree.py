5175196634f65506304b2a89f895fa484776f508
Ashish Bansal <bansal.ashish096@gmail.com>
2019-02-15 03:00:15 +0000

    Add tests for full/partial out of bound values

--- tests/test_segtree.py
+++ tests/test_segtree.py
@@ -50,5 +50,45 @@ class TestSegmentTree(TestCase):
         self.assertEqual(2, segtree.query_len(0, 8))
         self.assertEqual(2, segtree.query_sum(0, 8))
 
+    def test_full_out_of_bound(self):
+        segtree = SegmentTree(0, 8)
+        segtree.add(0, 8)
+
+        # Test full out of bound adding element fails
+        self.assertEqual(False, segtree.add(10, 16))
+        self.assertEqual(False, segtree.add(-16, -10))
+
+        # Test full out of bound len query returns 0
+        self.assertEqual(0, segtree.query_len(10, 16))
+        self.assertEqual(0, segtree.query_len(-16, -10))
+
+        # Test full out of bounds len query returns 0
+        self.assertEqual(0, segtree.query_sum(10, 16))
+        self.assertEqual(0, segtree.query_sum(-16, -10))
+
+        # Test full out of bounds max query returns None
+        self.assertEqual(None, segtree.query_max(10, 16))
+        self.assertEqual(None, segtree.query_max(-16, -10))
+
+    def test_partial_out_of_bound(self):
+        segtree = SegmentTree(0, 8)
+        segtree.add(0, 8)
+
+        # Test partial out of bound adding element
+        self.assertEqual(True, segtree.add(8, 16))
+        self.assertEqual(True, segtree.add(-16, 0))
+
+        # Test partial out of bound len query
+        self.assertEqual(2, segtree.query_len(7, 16))
+        self.assertEqual(2, segtree.query_len(-16, 1))
+
+        # Test partial out of bounds len query
+        self.assertEqual(3, segtree.query_sum(7, 16))
+        self.assertEqual(3, segtree.query_sum(-16, 1))
+
+        # Test full out of bounds max query
+        self.assertEqual(2, segtree.query_max(7, 16))
+        self.assertEqual(2, segtree.query_max(-16, 1))
+
 if __name__ == '__main__':
     main()
